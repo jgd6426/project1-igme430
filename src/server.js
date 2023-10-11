3aac5049425478e96e11e23c228ead2e5ca60bd1
Judy Derrick <jgd6426@rit.edu>
2023-10-11 14:03:29 -0400

    Update client html and css

--- src/server.js
+++ src/server.js
@@ -4,23 +4,25 @@ const htmlHandler = require('./htmlResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const handleGet = (request, response) => {
-    // route to correct method based on url
-    if (parsedUrl.pathname === '/style.css') {
-        htmlHandler.getCSS(request, response);
-    }
-    else {
-        htmlHandler.getIndex(request, response);
-    }
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else {
+    htmlHandler.getIndex(request, response);
+  }
 };
 
 const onRequest = (request, response) => {
-    // parse information from the url
-    const parsedUrl = url.parse(request.url);
+  // parse information from the url
+  const parsedUrl = url.parse(request.url);
 
+  if (request.method === 'GET') {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 // start server
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
